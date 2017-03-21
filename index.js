ea5e89b7f853cca1a1ce099273a3f1df676b01f1
Gabriel Lesperance <gabriel@wavo.me>
2017-03-21 16:34:08 -0400

    Fixed: handling Circular children with toJSON overidden (issue #13)

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ function decirc (val, k, stack, parent) {
   } else if (val instanceof Circle) {
     val.count++
     return
+  } else if (typeof val.toJSON === 'function') {
+    return
   } else if (parent) {
     if (~stack.indexOf(val)) {
       parent[k] = new Circle(val, k, parent)
