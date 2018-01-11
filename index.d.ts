a1ded41f99400140c4ba87c5d5d0ba1e7f397085
Ruben Bridgewater <ruben@bridgewater.de>
2018-01-11 15:58:36 +0100

    types: add namespace for stable

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
 declare function stringify(data: any): string;
 
+declare namespace stringify {
+  export declare function stable(data: any): string;
+  export declare function stableStringify(data: any): string;
+}
+
 export default stringify;
